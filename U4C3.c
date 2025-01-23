fa1091d9d8f71f4fbca5479dcca46417e0da066b
Reinan <reinanlopes.1@gmail.com>
2025-01-23 14:20:56 -0300

    Implementa leitura detalhada das teclas do teclado

--- U4C3.c
+++ U4C3.c
@@ -7,18 +7,47 @@
 
 #include "includes/teclado.h"
 
-
-
-int main()
-{
+int main() {
     stdio_init_all(); 
     keypad_init();
 
     while (true) {
        char key = keypad_read();
-        if (key != 0) {
-            printf("Key pressed: %c\n", key);
+       
+        if (key == '1') {
+            printf("Você pressionou 1\n"); // implementação da funcionalidade 1
+        } else if (key == '2') {
+            printf("Você pressionou 2\n"); // implementação da funcionalidade 2
+        } else if (key == '3') {
+            printf("Você pressionou 3\n"); // implementação da funcionalidade 3
+        } else if (key == '4') {
+            printf("Você pressionou 4\n"); // implementação da funcionalidade 4
+        } else if (key == '5') {
+            printf("Você pressionou 5\n"); // implementação da funcionalidade 5
+        } else if (key == '6') {
+            printf("Você pressionou 6\n"); // implementação da funcionalidade 6
+        } else if (key == '7') {
+            printf("Você pressionou 7\n"); // implementação da funcionalidade 7
+        } else if (key == '8') {
+            printf("Você pressionou 8\n"); // implementação da funcionalidade 8
+        } else if (key == '9') {
+            printf("Você pressionou 9\n"); // implementação da funcionalidade 9
+        } else if (key == '0') {
+            printf("Você pressionou 0\n"); // implementação da funcionalidade 10
+        } else if (key == 'A') {
+            printf("Você pressionou A\n"); // implementação da funcionalidade 11
+        } else if (key == 'B') {
+            printf("Você pressionou B\n"); // implementação da funcionalidade 12
+        } else if (key == 'C') {
+            printf("Você pressionou C\n"); // implementação da funcionalidade 13
+        } else if (key == 'D') {
+            printf("Você pressionou D\n"); // implementação da funcionalidade 14
+        } else if (key == '*') {
+            printf("Você pressionou *\n"); // implementação da funcionalidade 15
+        } else if (key == '#') {
+            printf("Você pressionou #\n"); // implementação da funcionalidade 16
+        } else {
+            printf("Tecla não reconhecida\n"); // implementação da funcionalidade 17
         }
-        sleep_ms(50);
     }
 }
